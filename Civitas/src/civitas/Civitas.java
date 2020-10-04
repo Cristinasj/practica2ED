bc217da0d5d55ea77940290bbd7a1e21c9e2d0dc
inowen <inoah@correo.ugr.es>
2020-10-04 10:05:28 +0200

    Initial commit

---
+++ Civitas/src/civitas/Civitas.java
@@ -0,0 +1,16 @@
+package civitas;
+
+/**
+ * 
+ * @author noah
+ */
+public class Civitas {
+
+   /**
+    * Main class
+    */
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Civitas");
+    }
+    
+}
---
+++ CivitasRuby/lib/main.rb
@@ -0,0 +1,21 @@
+module Civitas
+  
+  
+  
+  module TipoCasilla
+    CALLE = :calle
+    SORPRESA = :sorpresa
+    JUEZ = :juez
+    IMPUESTO = :impuesto
+    DESCANSO = :descanso
+  end
+  
+  module TipoSorpresa
+    IRCARCEL = :ircarcel
+    IRCASILLA = :ircasilla
+    PAGARCOBRAR = :pagarcobrar
+    PORCASAHOTEL = :porcasahotel
+    PORJUGADOR = :porjugador
+    SALIRCARCEL = :salircarcel
+  end
+end
