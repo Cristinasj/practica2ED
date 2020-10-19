3a9e8096c003bdb8afc2f38e058b0de2aa2babe7
inowen <inoah@correo.ugr.es>
2020-10-19 20:38:25 +0200

    Menu funcionante.

--- imagenES/imag.cpp
+++ imagenES/imag.cpp
@@ -1,5 +1,6 @@
 // El main del programa. Como el que será el menú, al que se le dan opciones y transforma las imágenes.
 
+
 #include <string>
 #include <iostream>
 #include <sstream>
@@ -10,6 +11,9 @@
 using namespace std;
 
 typedef unsigned char byte;
+
+// borrar esta línea (la necesito para que compile el menú sin haber terminado la parte del contraste)
+Imagen Imagen::aumenta_contraste(byte min, byte max) {}
 
 
 int main(int nargs, char ** args) {
@@ -32,8 +36,76 @@ int main(int nargs, char ** args) {
   ayuda << "\tParámetros: 2. Extremo menor y extremo mayor del intervalo de luminosidad deseado." << endl << endl;
   ayuda << "#-------------------------------#\n";
 
+  string menu_ayuda = ayuda.str();
 
-  cout << ayuda.str();
+  // Tiene que haber al menos 3 argumentos. La opción, el fichero original y donde guardar el resultado.
+  if (nargs < 3) {
+    cout << menu_ayuda << endl;
+    return 1;
+  }
+
+  string opcion = args[1];
+  string nombre_fuente = args[2];
+  string nombre_destino = args[3];
+
+  if (opcion == "umbralizar" && nargs == 4) {
+    Imagen im(nombre_fuente.c_str());
+    im.umbralizar_automaticamente();
+    im.escribir_imagen_a(nombre_destino.c_str());
+  }
+
+  else if (opcion == "icono" && nargs == 6) {
+    string num_filas = args[4];
+    string num_cols = args[5];
+
+    stringstream aux;
+    aux << num_filas << " " << num_cols;
+    int n_f, n_c; // Número de filas y columnas de la imagen deseada
+    aux >> n_f >> n_c; // Es FIFO
+
+    Imagen im(nombre_fuente.c_str());
+    Imagen icono = im.crear_icono(n_f, n_c);
+    icono.escribir_imagen_a(nombre_destino.c_str());
+  }
+
+  else if (opcion == "contraste" && nargs == 6) {
+    string min_s = args[4];
+    string max_s = args[5];
+    stringstream aux;
+    aux << min_s << " " << max_s;
+    int min, max;
+    aux >> min >> max;
+
+    // Hacer la conversión y escribir el resultado al archivo destino.
+    Imagen(nombre_fuente.c_str()).aumenta_contraste(min, max).escribir_imagen_a(nombre_destino.c_str());
+  }
+
+  else {
+    cout << menu_ayuda << endl;
+  }
+
+
+
 
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
