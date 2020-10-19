3d6c1f78f4cf8a687a5f8238397be989c91a5a34
inowen <inoah@correo.ugr.es>
2020-10-19 15:53:53 +0200

    Borrar las partes asociadas a imágenes de color

--- imagenES/imagenES.h
+++ imagenES/imagenES.h
@@ -1,8 +1,8 @@
 /**
   * @file imagenES.h
-  * @brief Fichero cabecera para la E/S de imágenes
+  * @brief Fichero cabecera para la E/S de imï¿½genes
   *
-  * Permite la E/S de archivos de tipo PGM,PPM
+  * Permite la E/S de archivos de tipo PGM
   *
   */
 
@@ -13,7 +13,7 @@
   * @brief Tipo de imagen
   *
   * Declara una serie de constantes para representar los distintos tipos
-  * de imágenes que se pueden manejar.
+  * de imï¿½genes que se pueden manejar.
   *
   * @see LeerTipoImagen
   */
@@ -29,46 +29,19 @@ enum TipoImagen {IMG_DESCONOCIDO, IMG_PGM, IMG_PPM};
   */
 TipoImagen LeerTipoImagen (const char *nombre);
 
-/**
-  * @brief Lee una imagen de tipo PPM
-  *
-  * @param nombre archivo a leer
-  * @param filas Parámetro de salida con las filas de la imagen.
-  * @param columnas Parámetro de salida con las columnas de la imagen.
-  * @return puntero a una nueva zona de memoria que contiene @a filas x @a columnas x 3
-  * bytes que corresponden a los colores de todos los píxeles en formato
-  * RGB (desde la esquina superior izqda a la inferior drcha). En caso de que no
-  * no se pueda leer, se devuelve cero. (0).
-  * @post En caso de éxito, el puntero apunta a una zona de memoria reservada en
-  * memoria dinámica. Será el usuario el responsable de liberarla.
-  */
-unsigned char *LeerImagenPPM (const char *nombre, int& fils, int& cols);
-
-/**
-  * @brief Escribe una imagen de tipo PPM
-  *
-  * @param nombre archivo a escribir
-  * @param datos punteros a los @a f x @a c x 3 bytes que corresponden a los valores
-  *    de los píxeles de la imagen en formato RGB.
-  * @param f filas de la imagen
-  * @param c columnas de la imagen
-  * @return si ha tenido éxito en la escritura.
-  */
-bool EscribirImagenPPM (const char *nombre, const unsigned char *datos, 
-                        const int fils, const int cols);
 
 /**
   * @brief Lee una imagen de tipo PGM
   *
   * @param nombre archivo a leer
-  * @param filas Parámetro de salida con las filas de la imagen.
-  * @param columnas Parámetro de salida con las columnas de la imagen.
+  * @param filas Parï¿½metro de salida con las filas de la imagen.
+  * @param columnas Parï¿½metro de salida con las columnas de la imagen.
   * @return puntero a una nueva zona de memoria que contiene @a filas x @a columnas
-  * bytes que corresponden a los grises de todos los píxeles
+  * bytes que corresponden a los grises de todos los pï¿½xeles
   * (desde la esquina superior izqda a la inferior drcha). En caso de que no
   * no se pueda leer, se devuelve cero. (0).
-  * @post En caso de éxito, el puntero apunta a una zona de memoria reservada en
-  * memoria dinámica. Será el usuario el responsable de liberarla.
+  * @post En caso de ï¿½xito, el puntero apunta a una zona de memoria reservada en
+  * memoria dinï¿½mica. Serï¿½ el usuario el responsable de liberarla.
   */
 unsigned char *LeerImagenPGM (const char *nombre, int& fils, int& cols);
 
@@ -77,14 +50,15 @@ unsigned char *LeerImagenPGM (const char *nombre, int& fils, int& cols);
   *
   * @param nombre archivo a escribir
   * @param datos punteros a los @a f x @a c bytes que corresponden a los valores
-  *    de los píxeles de la imagen de grises.
+  *    de los pï¿½xeles de la imagen de grises.
   * @param f filas de la imagen
   * @param c columnas de la imagen
-  * @return si ha tenido éxito en la escritura.
+  * @return si ha tenido ï¿½xito en la escritura.
   */
-bool EscribirImagenPGM (const char *nombre, const unsigned char *datos, 
+bool EscribirImagenPGM (const char *nombre, const unsigned char *datos,
                         const int fils, const int cols);
 
 #endif
 
-/* Fin Fichero: imagenES.h */
+/**************************************************************************/
+/**************************************************************************/
